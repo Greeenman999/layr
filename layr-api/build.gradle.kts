90c464d43966afe721879eacccc730126f0b6e87
Greenman999 <git@greenman999.de>
2026-01-17 15:52:52 +0100

    build(api): add JOML library dependency

--- layr-api/build.gradle.kts
+++ layr-api/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 }
 
 dependencies {
+	implementation("org.joml:joml:1.10.8")
+
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
